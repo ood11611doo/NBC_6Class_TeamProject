8ead076821f891e0482f105fa069137f4471e064
suj5453-star <suj5453@gmail.com>
2025-12-31 14:58:40 +0900

    251231 Monster coding
    Add "Recent Killed Monster"
    Do not spawn recent killed monster.

--- NBC_6Class_TeamProject/Monster.cpp
+++ NBC_6Class_TeamProject/Monster.cpp
@@ -5,34 +5,21 @@
 using namespace std;
 
 Monster::Monster(int plLevel) {
-	// - Todo : 생성자 정의 필요
-// 몬스터 스탯 설정 필요
-//  생성할 때 플레이어 레벨 기반으로 스탯 설정이 되기 때문에
-// 플레이어 레벨값을 무조건 받아서 몬스터를 생성하는 식이 필요
-// int randSize = rand() % monsterNames.size();
-// ㄴ이 위 식이 vector 랜덤 인덱스를 뽑아주니, 그걸로 사용하면 됨
-
+	
+	// 직전 몬스터이름을 저장해서 해당 몬스터 이름과 같으면 다시 뽑기
 	do {
 		int randSize = rand() % monsterNames.size();
 		MonsterNameIndex = randSize;
-		MonsterName = monsterNames[randSize]; // 랜덤 몬스터 생성
-		SameMonsterName = false;
+		MonsterName = monsterNames[MonsterNameIndex]; // 랜덤 몬스터 생성
 
-		for (const string& monsterName : KilledMonsterList) {
-			if (MonsterName == monsterName) {
-				SameMonsterName = true;
-				break;
-			}
+		SameWithRecentMonsterName = false;
+
+		if (MonsterName == RecentMonsterName) {
+			SameWithRecentMonsterName = true;
 		}
-	} while (SameMonsterName); 
-	
-	/*if (string monsterName : KilledMonsterList) {
-		monsterName == MonsterName;
-	}
-	KilledMonsterList.push_back(MonsterName);
-	*/
 
-	// 이미 처치한 몬스터 중복 소환 방지 필요함.
+	} while (SameWithRecentMonsterName);
+
 
 	int RandomValueHp = rand() % (11) + 20;
 	MonsterHp = plLevel * RandomValueHp;
@@ -43,18 +30,24 @@ Monster::Monster(int plLevel) {
 
 
 int Monster::TakeDamage(int damage) {
-	// - Todo : 기능 함수 정의 필요
-	// 체력 감소를 해당 함수에서 진행
-	// 그 후, 몬스터의 남은 체력을 return
-	// 0 이하면 0으로 return
+	
 	MonsterHp -= damage;
 	
 	if (MonsterHp <= 0) {
 		MonsterHp = 0;
+		RecentMonsterName = MonsterName;
 
-		KilledMonsterList.push_back(MonsterName);
 		return MonsterHp;
-	} // 몬스터 죽음 -> 여기에 구현?
+	}
 
 	return MonsterHp;
+}
+
+string Monster::PrintMonsterStatus() {
+	string ReturnString = "";
+	ReturnString += "몬스터 " + MonsterName + "등장!\n";
+	ReturnString += "현재 체력: " + to_string(MonsterHp) + "\n";
+	ReturnString += "공격력: " + to_string(MonsterAttack) + "\n";
+
+	return ReturnString;
 }
--- NBC_6Class_TeamProject/Monster.h
+++ NBC_6Class_TeamProject/Monster.h
@@ -6,29 +6,17 @@ using namespace std;
 
 class Monster {
 private:
-	// - Todo : 변수 선언 필요
-	// 
-	// string 값 이름 변수
-	// int값 몬스터 이름 가져온 인덱스
-	// int값 현재 체력
-	// int값 공격력
+
 	string MonsterName;
 	int MonsterNameIndex;
 	int MonsterHp;
 	int MonsterAttack;
-	vector<string> KilledMonsterList; // 처치한 몬스터 기록
-	bool SameMonsterName; // 몬스터 이름 중복검사
-public:
-	// - Todo : 생성자
-	// int 플레이어 레벨값 기반으로 스탯이 설정되기 때문에
-	// 아래 미리 선언되어있는 Monster값을 cpp파일에서 정의해주세요
-	Monster(int plLevel);
+	string RecentMonsterName; // 직전에 전투한 몬스터 이름
+	bool SameWithRecentMonsterName; // 직전 몬스터와 같은 이름이 나오지 않도록
 
-	// - Todo : getter, setter 함수 선언 필요
-	// 이름 함수 (getter, setter)
-	// 이름 인덱스 함수 (getter, setter)
-	// 현재 체력 함수 (getter, setter)
-	// 공격력 함수 (getter, setter)
+public:
+
+	Monster(int plLevel);
 
 	string GetMonsterName() { return MonsterName; }
 	void SetMonsterName(string name) { MonsterName = name; }
@@ -43,6 +31,7 @@ public:
 	int GetMonsterAttack() { return MonsterAttack; }
 	void SetMonsterAttack(int attack) { MonsterAttack = attack; }
 
-	// 기능 함수 선언됨 - 자세한 내용 정의는 cpp파일로
 	int TakeDamage(int damage);
+
+	string PrintMonsterStatus();
 };
