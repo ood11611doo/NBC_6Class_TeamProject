66b159e1362e907c67d51f0c5b4e45d323aa911c
이관식 <ood11611doo@gmail.com>
2026-01-05 10:50:54 +0900

    260105 - Update BuyItem for explanation
    아이템 구매 부분을 실제 사용가능하도록 수정.
    아이템 판매 부분을 추가 수정해주시면 될듯?

--- NBC_6Class_TeamProject/MarketSystem.cpp
+++ NBC_6Class_TeamProject/MarketSystem.cpp
@@ -6,38 +6,23 @@
 
 void MarketSystem::BuyItem(Player* pl, int itemIndex_, int buyCount_) {
 	if (pl == nullptr) { return; }
-	// 아이템벡터의 정보(주소나 개수)를 불러올 때 어떻게 ?
-	if (pl->getNumOfItem() != nullptr && itemIndex_ == 0 ) { // 플레이어의 아이템 벡터에 접근 불가. hpPotion 객체를 불러오는 방법?
-		if (pl->getGold() < ITEM_HEALING_PRICE * buyCount_) {
-			return;
-		}
-		else {
-			pl->setGold(pl->getGold() - (ITEM_HEALING_PRICE * buyCount_));// HealthPotion::AddCount(buyCount_); static 함수인 경우에만 유효함
-			HealthPotion* hpPotion; // non-static 멤버함수는 반드시 객체(this)가 필요함, 객체 따로 생성하면 안될듯?
-			hpPotion->AddCount(buyCount_);
-			item[0]->SetCount(pl->ReturnItemCount(0) + buyCount_); // 안됨...
-		}
+	int priceItem = 0;
+	if (pl->getNumOfItem()[itemIndex_] == nullptr) {
+		priceItem = GetBasicPrice(itemIndex_);
+	} else {
+		priceItem = pl->getNumOfItem()[itemIndex_]->GetPrice();
+	}
+	priceItem *= buyCount_;
 
-	}
-	else if (/*item[1] != nullptr*/ && itemIndex_ == 1) {
-		if (pl->getGold() < ITEM_BUFFDAMAGE_PRICE * buyCount_) {
-			return;
-		}
-		else {
-			pl->setGold(pl->getGold() - (ITEM_BUFFDAMAGE_PRICE * buyCount_));
-			AttackBoost* attBoost;
-			attBoost->AddCount(buyCount_);
-		}
-	}
-	else {
+	if (pl->getGold() < priceItem) {
 		return;
 	}
+
+	for (int i = 0; i < buyCount_; i++) {
+		pl->AddItemByIndex(itemIndex_);
+	}
+	pl->setGold(pl->getGold() - priceItem);
 }
-// Todo : 아이템 구매 함수
-		// Player를 포인터로 받아와서 사용하도록
-		// 여기서 안전수단으로 Player 골드와 구매하려는 아이템 및 구매수량 등을 체크해서
-		// 소유 골드보다 많을 경우 작동하지 않도록 해 주세요
-// 아이템 수량 증가 및 골드 차감도 여기서 이루어지도록 해 주세요
 
 void MarketSystem::SellItem(Player* pl, int itemIndex_, int sellCount) {
 	std::string item = "";
